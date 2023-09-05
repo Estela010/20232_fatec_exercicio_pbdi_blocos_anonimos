27fcb328b99cfa95e8a91e25a842bf038d6c6033
Gabriel <estela.moraes@fatec.sp.gov.br>
2023-09-05 09:07:23 -0300

    ap08(blocos-anonimos): codigo 2.4.1

--- script.sql
+++ script.sql
@@ -1,10 +1,23 @@
 DO
 $$
+DECLARE 
+	codigo INTEGER:= 1;
+	nome_completo VARCHAR (200):= 'João Santos';
+	-- 11 digitos no total , 2 para valores decimais 
+	salario NUMERIC (11,2):= 20.5;
 BEGIN
--- exibir uma mensagem
--- RAISE NOTICE "Meu primeiro bloco anônimo"
--- placeholdes (guardoão de lugar)
--- 2 + 2 = 4
-	RAISE NOTICE '% + % = %' , 2 ,2 , 2 + 2 ;
-END ; 
+	RAISE NOTICE 'meu código é % , meu nome é % e o meu salário é %' , codigo , nome_completo , salario ;
+END
 $$
+
+
+-- DO
+-- $$
+-- BEGIN
+-- -- exibir uma mensagem
+-- -- RAISE NOTICE "Meu primeiro bloco anônimo"
+-- -- placeholdes (guardoão de lugar)
+-- -- 2 + 2 = 4
+-- 	RAISE NOTICE '% + % = %' , 2 ,2 , 2 + 2 ;
+-- END ; 
+-- $$
